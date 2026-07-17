7eb672fbcb2f0111528f1587f0edd51cbda5f139
Zenkerice <zenangelopalay90@gmail.com>
2026-07-17 21:51:39 +0800

    added database.

--- inventory_db (25).sql
+++ inventory_db (25).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2026 at 03:31 PM
+-- Generation Time: Jul 17, 2026 at 03:47 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
